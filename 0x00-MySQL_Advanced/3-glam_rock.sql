64e97eaaac7c30e014f6f999341a87a0fdbbb51b
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-08 15:49:58 +0100

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,4 +3,4 @@
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
 
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan FROM metal_bands WHERE style REGEXP '.*Glam rock.*' ORDER BY lifespan DESC;
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan FROM metal_bands WHERE style REGEXP '.*Glam rock.*' ORDER BY lifespan DESC;
