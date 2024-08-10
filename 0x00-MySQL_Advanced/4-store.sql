2c79c1d0988a63876eeaf4b5d585e5f2c183d560
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-10 22:15:35 +0100

    Task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,16 +1,29 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 
+--DELIMITER $$
+--CREATE TRIGGER updat_quantity
+--AFTER INSERT ON orders
+--FOR EACH ROW
+--BEGIN
+--	UPDATE items
+--	INNER JOIN (
+--		    SELECT item_name, SUM(number) AS total_orders
+---		    FROM orders
+--		    GROUP BY item_name
+--	) AS order_totals ON items.name = order_totals.item_name
+--	SET items.quantity = items.quantity - order_totals.total_orders
+--	WHERE order_totals.item_name = items.name;
+--END$$
+--iDELIMITER;
 DELIMITER $$
-CREATE TRIGGER updat_quantity
+
+CREATE TRIGGER update_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
 	UPDATE items
-	INNER JOIN (
-		    SELECT item_name, SUM(number) AS total_orders
-		    FROM orders
-		    GROUP BY item_name
-	) AS order_totals ON items.name = order_totals.item_name
-	SET items.quantity = items.quantity - order_totals.total_orders;
+	SET items.quantity = items.quantity - NEW.number
+	WHERE items.name = NEW.item_name;
 END$$
-DELIMITER;
+
+DELIMITER ;
